a8b298fa19828ac70b7a8b5f211a591e322392bb
DD94 <37007020+DD94@users.noreply.github.com>
2018-03-04 12:12:49 +1100

    Itemgen.cs and PickupController.cs
    Two new scripts added to "test" scene.

---
+++ Hide and Seek Prototype/Assets/Scripts/ItemGen.cs
@@ -0,0 +1,40 @@
+﻿/**************
+ * ItemGen.cs
+ * Version 1.0
+ * Created by Dion Drake
+ * Last Edited: 03/03/2018
+*/
+
+//Instructions: Must be as many GameObjects declared as there are intended props. In Inspector, set each GameObject to an intended item. Now add a case to the 
+//nested if-else, e.g. if (rndNumGen == 3), if (rndNumGen == 4). The purpose of the second line in each statement is to immediately rename it upon instantiation, as 
+//it will be given a name like "Cube(Clone)" whilst we just want it to be named "Cube".
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemGen : MonoBehaviour {
+	public GameObject chosenObjToGen;
+	public GameObject objToGen1;
+	public GameObject objToGen2;
+
+	void Start () {
+		Random rnd = new Random();
+		int rndNumGen = Random.Range(1,3);
+		if (rndNumGen == 1) {
+			chosenObjToGen = Instantiate (objToGen1, transform.position + (transform.forward * 2), transform.rotation);
+			chosenObjToGen.name = objToGen1.name;
+		} else 
+			if (rndNumGen == 2) {
+			chosenObjToGen = Instantiate (objToGen2, transform.position + (transform.forward * 2), transform.rotation);
+			chosenObjToGen.name = objToGen2.name;
+		}
+		//else if continue here for more GameObjects
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Hide and Seek Prototype/Assets/Scripts/PickupController.cs
@@ -0,0 +1,68 @@
+/**************
+ * PickupController.cs
+ * Version 1.1
+ * Created by Dion Drake
+ * Last Edited: 03/03/2018
+*/
+
+//To use - place "Pickup" tag/add collider on obj intended to pickup. obj should have collider/rigidbody. Place on MainCamera object.
+
+//Importing namespaces - collections of items
+using UnityEngine; 
+using System.Collections; 
+using UnityEngine.UI; 
+using UnityEngine.EventSystems; 
+
+public class PickupController : MonoBehaviour
+{
+	private bool isLookingAtObj;
+	private bool hasAcquiredObj;
+	private GameObject aimedAt;
+	private string aimedObj_name;
+	private string acquiredObj_name;
+	private float startTime = 0;
+	//Runs on script load
+	public void Start()
+	{
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+	}
+
+	public void Update()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast(ray, out hit)){
+			aimedAt = hit.collider.gameObject;
+			if (hit.collider.tag == "Pickup" && hit.distance <= 2 && aimedAt.name != "Terrain") {
+					isLookingAtObj = true;
+				if (Input.GetKeyDown ("e")) {
+					acquiredObj_name = aimedAt.name;
+					Destroy (aimedAt);
+					hasAcquiredObj = true;
+					isLookingAtObj = false;
+				}
+			}
+			else
+			{
+				isLookingAtObj = false;
+			}
+		}
+	}
+
+	public void OnGUI()
+	{
+		if (isLookingAtObj) {
+			GUI.Box(new Rect (140, Screen.height - 50, Screen.width - 300, 120), "Press E to acquire " + aimedAt.name);
+		}
+		if (hasAcquiredObj) {
+			GUI.Box(new Rect (140, Screen.height - 50, Screen.width - 300, 120), "Acquired " + acquiredObj_name);
+			if (startTime < 5) {
+				startTime += Time.deltaTime;
+			} else {
+				hasAcquiredObj = false;
+				startTime = 0;
+			}
+		}
+	}
+	}
