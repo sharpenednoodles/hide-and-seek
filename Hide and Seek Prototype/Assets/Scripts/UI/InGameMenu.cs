10cd656c616a0fdb203cfde485c787b457c57182
Unknown <Lyhuy55@gmail.com>
2018-05-12 22:34:29 +1000

    Escape Menu Interface

---
+++ Hide and Seek Prototype/Assets/Scripts/UI/InGameMenu.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour {
+
+
+	public Transform Menu;
+	public static bool MenuState = false;
+
+	//public GameObject Menu;
+
+	// Use this for initialization
+	void Start () {
+
+		Menu.gameObject.SetActive (false);
+		//Menu.SetActive(false);
+		MenuState = false;
+	}
+
+
+//	void updateMenu(){
+//
+//		if (Input.GetKeyDown(KeyCode.M)) {
+//
+//
+//						if (Menu.gameObject.activeSelf == false) {
+//
+//			me
+//			Menu.gameObject.SetActive (true);
+//			Debug.Log ("Set Active");
+//						} 
+//						else {
+//			
+//							Menu.gameObject.SetActive (false);
+//							Debug.Log ("Disable");
+//						}
+//		}
+//
+//	}
+
+	void showMenu()
+	{
+		Menu.gameObject.SetActive (true);
+		//Menu.SetActive(true);
+		Debug.Log ("Set Active");
+		MenuState = true;
+	}
+
+	void hideMenu()
+	{
+		Menu.gameObject.SetActive (false);
+		//Menu.SetActive(false);
+		MenuState = false;
+		Debug.Log ("Set Disable");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("refresh key");
+		if (Input.GetKey(KeyCode.M)) {
+
+
+			if (MenuState) {
+				
+				hideMenu();
+				Debug.Log ("Call From hide");
+
+
+
+
+			} else 
+			{ 
+				showMenu ();
+				Debug.Log ("Call From show");
+
+			}
+
+
+	}
+
+	}
+}
